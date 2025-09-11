cdae1e34d9e7bbc3f087336f5dbefe595c4b966e
gopherss <sandrogopher@gmail.com>
2025-09-11 14:40:44 -0500

    feature notification-service

--- notification-service/src/main/java/com/escuelajavag4/notification_service/model/Notification.java
+++ notification-service/src/main/java/com/escuelajavag4/notification_service/model/Notification.java
@@ -1,4 +1,36 @@
 package com.escuelajavag4.notification_service.model;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "notification")
+
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long notificationId;
+
+    @Column
+    private String email;
+
+    @Column
+    private String sms;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Date  updatedAt;
 }

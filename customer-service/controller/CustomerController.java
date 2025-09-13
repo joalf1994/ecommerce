4e5905cada012bc04be192a8e80dc0a20e40f7f1
Carlos D. Adrianzen Adanaque <carlos.adrianzen27972@gmail.com>
2025-09-13 12:58:11 -0500

    Agregar nueva estructura de Customer Service

---
+++ customer-service/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.escuelajavag4.customer_service.controller;
+
+import com.escuelajavag4.customer_service.dto.CustomerDto;
+import com.escuelajavag4.customer_service.service.ICustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final ICustomerService customerService;
+
+    @GetMapping("/{id}")
+    public CustomerDto getCustomerById(@PathVariable Long id) {
+        return customerService.findById(id);
+    }
+
+    @GetMapping("/search")
+    public CustomerDto getCustomerByEmail(@RequestParam String email) {
+        return customerService.findByEmail(email);
+    }
+}

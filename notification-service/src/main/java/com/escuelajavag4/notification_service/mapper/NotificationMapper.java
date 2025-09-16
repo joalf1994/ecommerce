4a90996731d4135254db212a26586760aab8ad1d
gopherss <sandrogopher@gmail.com>
2025-09-16 00:51:42 -0500

    Implementación de notification-service: - Consumidor Kafka para eventos ORDER_CONFIRMED - Endpoint REST para consultar notificaciones por orderId

--- notification-service/src/main/java/com/escuelajavag4/notification_service/mapper/NotificationMapper.java
+++ notification-service/src/main/java/com/escuelajavag4/notification_service/mapper/NotificationMapper.java
@@ -1,4 +1,13 @@
 package com.escuelajavag4.notification_service.mapper;
 
-public class NotificationMapper {
+import com.escuelajavag4.notification_service.model.dto.response.NotificationDto;
+import com.escuelajavag4.notification_service.model.entity.NotificationEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface NotificationMapper {
+
+    NotificationDto toDto(NotificationEntity notificationEntity);
+
+    NotificationEntity toEntity(NotificationDto notificationDto);
 }

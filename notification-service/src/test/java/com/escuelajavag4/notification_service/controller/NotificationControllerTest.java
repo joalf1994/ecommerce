71d9c685b556a05048f92eb772bea9bfe8c156c8
gopherss <sandrogopher@gmail.com>
2025-09-19 00:14:01 -0500

    notification comp

--- notification-service/src/test/java/com/escuelajavag4/notification_service/controller/NotificationControllerTest.java
+++ notification-service/src/test/java/com/escuelajavag4/notification_service/controller/NotificationControllerTest.java
@@ -7,7 +7,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.Instant;
@@ -21,7 +21,7 @@ public class NotificationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private INotificationService notificationService;
 
     @Test

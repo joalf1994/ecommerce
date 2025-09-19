c6eab547c8531572fc6ea20944904df3ca6e62a6
Jbobadilla <joalf1994@gmail.com>
2025-09-19 12:33:42 -0500

    modificacion de la entidad customer

--- customer-service/src/main/java/com/escuelajavag4/customer_service/model/Customer.java
+++ customer-service/src/main/java/com/escuelajavag4/customer_service/model/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
     private String fullName;
